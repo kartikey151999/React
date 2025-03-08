5b2c7a9061ba8911d543b44b82ed3d99eb7f86be
Kartikey Pande <71623412+kartikey151999@users.noreply.github.com>
2025-03-08 11:53:35 +0530

    Update constant.js

--- Ep5(Food Order App - 2)Hooks/src/utils/constant.js
+++ Ep5(Food Order App - 2)Hooks/src/utils/constant.js
@@ -1,5 +1,2 @@
-
 export const logo_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzfmJwrfLdQVZhChBzZ2f0xa2wAy9hOXFcGw&s"
-
-
-export const image_url = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+export const Resimage_url = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
